bcf8e9596422ed8087782d122dedbc6640044f40
Niko Sandschneider <nsandschn@gmx.de>
2019-05-04 16:23:50 +0200

    Initial commit

---
+++ test.py
@@ -0,0 +1,31 @@
+import numpy as np
+from optimizers import StupidGradientDescent
+from functions import Rosenbrock
+
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+mpl.style.use('seaborn')
+
+BUDGET = 1000
+
+if __name__=="__main__":
+    # Initialize benchmark function object
+    fr = Rosenbrock()
+    # Use deterministic randomness
+    np.random.seed(seed=42)
+    # Initialize optimization algorithm object
+    initstate = 10*(np.random.rand(2,  1)-1)[:,  0]
+    gd = StupidGradientDescent(0.0001,  initstate, fr.grad)
+
+    # Numpy array for logging
+    performance = np.zeros((BUDGET, ))
+    
+    for i in range(BUDGET):
+        # Take a gradient step with constant alpha
+        gd.step()
+        # Save f value
+        performance[i] = fr.eval(gd.state[0], gd.state[1])
+
+    plt.plot(np.log10(performance), 'bo')
+    plt.savefig("rosenbrock.png", dpi=200)
+    #plt.show()
